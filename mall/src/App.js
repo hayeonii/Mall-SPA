fe11d67a64a7d4ef273a29120f0aa4b660ccb2fb
hayeonii <hayexn51@gmail.com>
2022-12-14 00:15:00 +0900

    feat: router 경로 설정

--- mall/src/App.js
+++ mall/src/App.js
@@ -1,6 +1,10 @@
 import Main from "./pages/Main/Main";
-import { createGlobalStyle } from 'styled-components'
-import reset from 'styled-reset'
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import useGetData from "./hooks/useGetData";
+import CartAndPayment from "./pages/CartAndPayment/CartAndPayment";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -8,14 +12,21 @@ const GlobalStyle = createGlobalStyle`
     box-sizing:border-box;
     font-family:'Spoqa Han Sans Neo', sans-serif;
   }
-`
-
+`;
 
 function App() {
+  const data = useGetData("https://test.api.weniv.co.kr/mall");
+
   return (
     <>
-      <GlobalStyle/>
-      <Main/>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main data={data} />} />
+          <Route path="/product/:id" element={<ProductDetails data={data} />} />
+          <Route path="/cart" element={<CartAndPayment />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
